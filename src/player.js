2cc4a5580d0575b4b956612aac60eefbba2be690
KetillG <ketillgudmunds+github@gmail.com>
2018-02-08 18:21:04 +0000

    First working copy made

--- src/player.js
+++ src/player.js
@@ -7,19 +7,51 @@ const testBoard =
     [0,1,0,0,0]
 ];
 
-transposeBoard = (board) => board.map((col, i) => board.map(row => row[i]));
+computeBestMove = (board, player) => {
+    const topMove = computeMoveForRow(deepCopyBoard(board), 1, player);
+    const midMove = computeMoveForRow(deepCopyBoard(board), 2, player);
+    const botMove = computeMoveForRow(deepCopyBoard(board), 3, player);
 
-// Always treats current player as the player moving horizontal
-getBestMove = (board, player) => {
-    const topMove = getNextMove(board[1], player);
-    const midMove = getNextMove(board[2], player);
-    const botMove = getNextMove(board[3], player);
     console.log(topMove, midMove, botMove);
-
-    // 
+    
 }
 
 
+// Always treats current player as the player moving horizontal
+getBestMove = (board, player) => {
+    
+    const topMove = computeMoveForRow(deepCopyBoard(board), 1, player);
+    const midMove = computeMoveForRow(deepCopyBoard(board), 2, player);
+    const botMove = computeMoveForRow(deepCopyBoard(board), 3, player);
+    
+    const score = topMove + midMove + botMove;
+    console.log(score);
+    return score
+}
+
+computeMoveForRow = (board, rowIndex, player) => {
+    const row = board[rowIndex];
+    // Next position
+    const nextMove = getNextMove(row, player);
+    // If end hit then exit
+    
+    if(nextMove === -1) return 0;
+    // Current position
+    playerPosition = getPlayerPosition(row, player);
+    // Update position
+    
+    board[rowIndex][playerPosition] = 0;
+    board[rowIndex][nextMove] = player;
+    
+    // Check if this won the game
+    if(checkIfWon(board)) return 1;
+    
+    // Switch players and go deeper into game tree
+    console.log('kkk');
+    
+    
+    return -getBestMove(transposeBoard(board), getNextPlayer(player));
+}
 // Returns next move for the player
 getNextMove = (row, player) => {
     playerPosition = getPlayerPosition(row, player);
@@ -30,6 +62,9 @@ getNextMove = (row, player) => {
     return -1;
 }
 
+// Flips the array
+transposeBoard = (board) => board.map((col, i) => board.map(row => row[i]));
+
 // Current position
 getPlayerPosition = (row, player) => row.indexOf(player);
 
@@ -39,4 +74,7 @@ checkIfWon = (board) => board[1][4] !== 0 && board[2][4] !== 0 && board[3][4] !=
 // Returns next player
 getNextPlayer = (player) => player === 1 ? 2 : 1;
 
-getBestMove(testBoard, 2)
+// Copies array of array with dereferencing
+deepCopyBoard = (board) => board.map(row => [...row]);
+
+computeBestMove(testBoard, 2)
